ee29db553da82188c91b8d674b29568e87c63bc0
Michel Roberto Traña Tablada <mtraatabladaa94@gmail.com>
2020-08-20 11:49:56 -0600

    Contenido para vídeo sobre useReducer

--- src/frontend/characters/list.js
+++ src/frontend/characters/list.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {LOADING, ERROR,} from './index-with-context';
 
 export const List = (props) => {
     const {
@@ -8,9 +9,17 @@ export const List = (props) => {
         next,
         prev,
         connected,
+        statusData,
+        error,
     } = props;
     return(
         <>
+            {statusData === LOADING && (
+                <p>Cargando datos...</p>
+            )}
+            {statusData === ERROR && (
+                <p>{error.message}</p>
+            )}
             <ul>
                 {data && data.map(item => {
                     return (<li key={item.id}>{item.name}</li>);
--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import {Suscripcion} from './suscripcion';
-import {Characters} from './characters';
+import {Characters} from './characters/index-with-context';
 
 ReactDOM.render(<Characters />, document.getElementById('app-react'));
